44f1ac298ed54286c93879cfbaa74d67fc7d4681
Mishr1233 <shivshankarmishra84351935@gmail.com>
2025-01-08 06:19:48 +0530

    Addition function

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Addition{
+public static void main(String args[]){
+
+Scanner sc=new Scanner (System.in);
+System.out.println("Enter a no :");
+int a=sc.nextInt();
+
+System.out.println("Enter second no :");
+int b=sc.nextInt();
+int c=a+b;
+
+System.out.println("Sum is : "+c);
+}
+
+}
